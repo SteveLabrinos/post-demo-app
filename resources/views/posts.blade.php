c8b56bb00e6ee650b2be1a45796a7c7669940745
SteveLabrinos <slabrinos9214@gmail.com>
2021-10-27 21:51:55 +0300

    add dynamic routes to every post

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -5,21 +5,21 @@
 
 <body>
     <article>
-        <h1><a href="/post">My First Post</a></h1>
+        <h1><a href="/posts/my-first-post">My First Post</a></h1>
         <p>
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci dicta distinctio doloribus ducimus eligendi eos est excepturi facilis iste nam odio omnis, optio, perferendis porro quasi sed sint sit sunt temporibus veniam veritatis vero voluptate? Aliquid aperiam beatae dignissimos, dolore eos fugiat incidunt ipsum laborum nobis quae sit tempore unde vel, vero voluptatum! A ad aspernatur commodi doloremque, earum error et ex excepturi impedit incidunt laboriosam maiores, minima modi necessitatibus nobis odit praesentium quia quisquam quos reiciendis rerum saepe vitae!
         </p>
     </article>
 
     <article>
-        <h1><a href="/post">My Second Post</a></h1>
+        <h1><a href="/posts/my-second-post">My Second Post</a></h1>
         <p>
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci dicta distinctio doloribus ducimus eligendi eos est excepturi facilis iste nam odio omnis, optio, perferendis porro quasi sed sint sit sunt temporibus veniam veritatis vero voluptate? Aliquid aperiam beatae dignissimos, dolore eos fugiat incidunt ipsum laborum nobis quae sit tempore unde vel, vero voluptatum! A ad aspernatur commodi doloremque, earum error et ex excepturi impedit incidunt laboriosam maiores, minima modi necessitatibus nobis odit praesentium quia quisquam quos reiciendis rerum saepe vitae!
         </p>
     </article>
 
     <article>
-        <h1><a href="/post">My Third Post</a></h1>
+        <h1><a href="/posts/my-third-post">My Third Post</a></h1>
         <p>
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci dicta distinctio doloribus ducimus eligendi eos est excepturi facilis iste nam odio omnis, optio, perferendis porro quasi sed sint sit sunt temporibus veniam veritatis vero voluptate? Aliquid aperiam beatae dignissimos, dolore eos fugiat incidunt ipsum laborum nobis quae sit tempore unde vel, vero voluptatum! A ad aspernatur commodi doloremque, earum error et ex excepturi impedit incidunt laboriosam maiores, minima modi necessitatibus nobis odit praesentium quia quisquam quos reiciendis rerum saepe vitae!
         </p>
