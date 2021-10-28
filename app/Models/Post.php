9cb8e2728852f01062ca5e4044f333b1bdd0d51f
SteveLabrinos <slabrinos9214@gmail.com>
2021-10-28 21:49:59 +0300

    protect post values from mass assignment and remove all the post snippets

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    // Place all the properties that are protected from mass assignment
+    protected $guarded = ['id'];
+    // Alternatively place all the properties that are allowed for mass assignment
+//    protected $fillable = ['title', 'excerpt', 'body'];
+
+    // The third option is to ignore the guarded or fillable rules and never
+    // allow the create method for any mass assignment
 }
